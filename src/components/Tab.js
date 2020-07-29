90fe7a463c4d9ec3f4eb14fc09ffac4242087f22
Tai Hoang <tahoan@microsoft.com>
2020-07-29 17:21:13 -0400

    fix some UX stuff with microcade

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import * as microsoftTeams from "@microsoft/teams-js";
 
 /**
@@ -10,39 +10,42 @@ import * as microsoftTeams from "@microsoft/teams-js";
  * of your app.
  */
 class Tab extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      context: {}
-    }
+      context: {},
+    };
   }
 
   //React lifecycle method that gets called once a component has finished mounting
   //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
+  componentDidMount() {
     // Get the user context from Teams and set it in the state
     microsoftTeams.getContext((context, error) => {
       this.setState({
-        context: context
+        context: context,
       });
     });
     // Next steps: Error handling using the error object
   }
 
   render() {
+    let userName =
+      Object.keys(this.state.context).length > 0
+        ? this.state.context["loginHint"]
+        : "";
 
-      let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+    let groupId =
+      Object.keys(this.state.context).length > 0
+        ? this.state.context["groupId"]
+        : "";
 
-      return (
+    let srcUrl = "https://7b37af001cff.ngrok.io/home?userId=" + userName + '&groupId=' + groupId;
+    return (
       <div>
-        <h1>ITS OVER {userName}</h1> <h3>TIME TO PLAY TETRIS :-)</h3>
-        <div>Tetris</div>
-        <div>Drawful</div>
-        <div>Uno</div>
-        <div></div>
-        <iframe src="https://b0d42084287c.ngrok.io/tab" width="540" height="450"></iframe>
+        <iframe src={srcUrl} width="100%" height="1080px"></iframe>
       </div>
-      );
+    );
   }
 }
 export default Tab;
